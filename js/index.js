b424ec487cadbaecf8ac15d2c48b5c64e629f71d
aleprimeGamesLtd <aleprimegames@gmail.com>
2023-12-04 19:14:12 -0400

    First commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let buttonReg = document.getElementById("btn-reg")
+let buttonLogin = document.getElementById("btn-login")
+
+buttonReg.addEventListener("click", () => {
+    document.open("./ingresar.html", "_parent", "")
+})
+
+buttonLogin.addEventListener("click", () => {
+    document.open("./ingresar.html", "_parent", "")
+})
---
+++ js/login.js
@@ -0,0 +1,10 @@
+let btnCancelar = document.getElementById("cancelar")
+let btnIngresar = document.getElementById("ingresar")
+
+btnCancelar.addEventListener("click", () => {
+    document.open("./index.html", "_parent", "")
+})
+
+btnIngresar.addEventListener("click", () => {
+    document.open("./index.html", "_parent", "")
+})
